da0da9cb3f3cc181c68e951fcea091da19e4ff42
ultratavor <felixtonthat64@gmail.com>
2024-05-12 00:12:54 +0200

    Update DataManager.class.js

--- script/web/DataManager.class.js
+++ script/web/DataManager.class.js
@@ -1,15 +1,39 @@
+import { isSet } from "util/types"
+
 /**
  * gestion des données par fréquence (QTD)
  */
-export class DataManager{
-    constructor (parameters) {
-        
+export class LotoDataAnalyse{
+
+    #titles = []
+    #data = []
+
+    constructor (anyFormatData,title) {
+        this.#data.push(anyFormatData)
+        this.#titles.push(title)
+    }
+
+    /**
+     * autorise la supperposition avec une couleur différente
+     * @param {*} data données a jouter
+     * @param {*} title titre des données
+     */
+    addData(data,title){
+        this.constructor(data,title)
     }
 
     get frequency(){
         var res = []
-
-
+        for (let i = 0; i < array.length; i++) {
+            if(isSet(res[i]))
+                res[i]++
+            else
+                res[i]=1
+        }
         return res
+    }
+
+    rendering(ctx){
+        // use ChartJS
     }
 }
