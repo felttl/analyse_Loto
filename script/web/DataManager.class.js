6a23cba86645d65fe580eec542f36819a2bfe458
ultratavor <felixtonthat64@gmail.com>
2024-05-11 23:54:28 +0200

    gestion des données

---
+++ script/web/DataManager.class.js
@@ -0,0 +1,15 @@
+/**
+ * gestion des données par fréquence (QTD)
+ */
+export class DataManager{
+    constructor (parameters) {
+        
+    }
+
+    get frequency(){
+        var res = []
+
+
+        return res
+    }
+}
