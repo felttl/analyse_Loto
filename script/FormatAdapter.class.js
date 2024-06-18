945dd6bab6f7fe07ca0c6edff7e7b09e3e67d66b
ultratavor <felixtonthat64@gmail.com>
2024-06-18 18:10:32 +0200

    classe de formattage customisée (avec best-practice) (du csv -> json (structure spéciale))

---
+++ script/FormatAdapter.class.js
@@ -0,0 +1,22 @@
+
+class FormatAdapter {
+
+    #crudeData=""
+
+    constructor (filepath) {
+        // recuperation des données
+
+        // a faire 
+
+    }
+
+
+    convert2json(){
+        let jsonText=null
+        // a faire
+        return JSON.parse(jsonText)
+    }
+}
+
+
+// end page
