63c3f56b1971c66db967b563ed06e9133433e169
felttl <68735155+felttl@users.noreply.github.com>
2024-06-27 15:16:07 +0200

    Update fileManager.php

--- scripts/php/fileManager/fileManager.php
+++ scripts/php/fileManager/fileManager.php
@@ -6,10 +6,6 @@ function csv2Json($filepath) {
     $result = array_map("str_getcsv", $csvLineArray);
     return "'".json_encode($result)."'";
 }
-function csv2Json2($filepath) {
-    $csvFileContent= file_get_contents($filepath);
-    $result = explode("\n", $csvFileContent);
-    return "'".json_encode($result)."'";
-}
+// preg_split("/,|;/",$a)
 
 // end page
