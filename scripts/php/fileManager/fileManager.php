3159d88fce35bd82a046937eb37a1dfd5b2c9b81
felttl <68735155+felttl@users.noreply.github.com>
2024-07-02 12:47:45 +0200

    Update fileManager.php

--- scripts/php/fileManager/fileManager.php
+++ scripts/php/fileManager/fileManager.php
@@ -3,17 +3,13 @@
 function csv2Json($filepath) {
     $csvFileContent= file_get_contents($filepath);
     $csvLineArray = explode("\n", $csvFileContent);
-    // peutêtre suppression du map a cause des erreur de lignes
-    $result = array_map("str_getcsv", $csvLineArray);
-    // correction des irregularites : (uniquement la première ligne a des ;)
-    // for ($i=1; $i < sizeof($result); $i++) { 
-    //     $tmp = $result[$i][sizeof($result)-1];
-    //     echo $tmp;
-    //     array_pop($result[$i][$result[$i]-1]);
-    //     $result[$i]=array_merge($result[$i],preg_split("/,|;/",$tmp));
-    // }
-    return "'".json_encode($result)."'";
+    //  suppression de map() a cause des erreur de lignes
+    for ($i=0; $i<sizeof($csvLineArray) ; $i++) { 
+        $csvLineArray[$i]=preg_split("/,|;/",$csvLineArray[$i]);
+        Array_pop($csvLineArray[$i]);
+    }
+    Array_pop($csvLineArray);
+    return "`".json_encode($csvLineArray)."`";
 }
-// preg_split("/,|;/",$a)
 
 // end page    
