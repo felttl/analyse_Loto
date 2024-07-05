976131a546b51dddbb832f364622ff5a88515ebc
felttl <68735155+felttl@users.noreply.github.com>
2024-07-05 16:28:14 +0300

    outils basique manquants cruellement

---
+++ scripts/js/maths+/basics.js
@@ -0,0 +1,18 @@
+
+/**
+ * module pour les compléments en maths
+ */
+
+
+/**
+ * get random numbers with intervall ([min,max[)
+ * @param {int} min int number included
+ * @param {int} max int number excluded
+ * @returns 
+ */
+function randi(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+
+//end page
