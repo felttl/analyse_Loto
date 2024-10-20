33c840e6c48719b9996eae9549d97058cd5c6a2f
felttl <68735155+felttl@users.noreply.github.com>
2024-10-20 15:50:42 +0200

    rajout DP décorateur poir chaque type d'analyse de données

---
+++ scripts/js/DailyTemplate.js
@@ -0,0 +1,17 @@
+
+/**
+* implemente le DP décorateur 
+*/
+class DailyTemplate extends DataTemplate implements DataManager{
+
+
+
+   constructor(origin){
+       this.deco=origin
+   }
+
+   //implements methods here
+
+}
+
+// end
